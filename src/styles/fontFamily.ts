a6537a56114109636244beb8ab8b28639b411335
Júlio Cézar <jczrpereira77@gmail.com>
2024-08-04 09:35:42 -0300

    style: color palette and font

---
+++ src/styles/fontFamily.ts
@@ -0,0 +1,8 @@
+export const fontFamily = {
+  light: "Quicksand_300Light",
+  regular: "Quicksand_400Regular",
+  medium: "Quicksand_500Medium",
+  semibold: "Quicksand_600SemiBold",
+  bold: "Quicksand_700Bold",
+}
+
